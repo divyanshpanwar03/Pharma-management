e84ff55746bbc4579a691911157fadb13fe6f060
Divyansh Panwar <166502767+divyanshpanwar03@users.noreply.github.com>
2024-04-27 13:03:43 +0530

    Add files via upload

---
+++ Validator.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author welcome
+ */
+public interface Validator{
+    void passwordValidator(String password) ;
+    void phoneValidator(String phone);
+}
